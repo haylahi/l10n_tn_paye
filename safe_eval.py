3de76f24d54410298ae4e2f8c1e5980df7458775
haylahi <cplus.contact@gmail.com>
2013-07-14 00:27:37 +0100

    Update safe_eval.py

--- safe_eval.py
+++ safe_eval.py
@@ -3,9 +3,9 @@ Overriding eval function defined in the base module
 in order to complete globals_dict with missing Python __builtins__
 """
 
-from hr_payroll import hr_salary_rule
+from hr_payroll import hr_payroll
 
-native_safe_eval = hr_salary_rule.eval
+native_safe_eval = hr_payroll.eval
 
 
 def new_safe_eval(expr, globals_dict=None, locals_dict=None, mode="eval", nocopy=False):
@@ -27,4 +27,4 @@ def new_safe_eval(expr, globals_dict=None, locals_dict=None, mode="eval", nocopy
     })
     return native_safe_eval(expr, globals_dict, locals_dict, mode, nocopy)
 
-hr_salary_rule.eval = new_safe_eval
+hr_payroll.eval = new_safe_eval
